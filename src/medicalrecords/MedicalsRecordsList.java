090d44813b2f043a824a925b544314e93fd52677
isaac <isaacpr11371@gmail.com>
2026-08-14 16:57:32 -0600

    agregacion de la clase MedicalsRecordsList con sus metodos completos
    creacion de los metodos add, remove, getAll y constructor

---
+++ src/medicalrecords/MedicalsRecordsList.java
@@ -0,0 +1,57 @@
+package medicalrecords;
+
+import WaitingRooms.SequentialDynamicsList;
+import java.util.EmptyStackException;
+import java.util.Iterator;
+import java.util.Stack;
+
+
+public class MedicalsRecordsList implements SequentialDynamicsList<MedicalRecord>{
+private Stack<MedicalRecord> medicalHistory;
+
+    public MedicalsRecordsList(Stack<MedicalRecord> medicalHistory) {
+        this.medicalHistory = new Stack();
+    }
+
+    @Override
+    public MedicalRecord get() {
+       try{
+           return medicalHistory.peek();
+       }catch(EmptyStackException e){
+          return null;
+       }
+    }
+
+    @Override
+    public boolean remove() {
+        try{
+           medicalHistory.pop();
+       }catch(EmptyStackException e){
+          return false;
+       }
+        return true;
+    }
+
+    @Override
+    public boolean add(MedicalRecord item) {
+        return medicalHistory.push(item)!=null;
+    }
+
+    @Override
+    public Iterator getAll() {
+        if (medicalHistory.empty()) return null;
+           return medicalHistory.iterator();
+        
+    }
+
+    @Override
+    public int size() {
+        return medicalHistory.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return medicalHistory.isEmpty();
+    }
+
+}
