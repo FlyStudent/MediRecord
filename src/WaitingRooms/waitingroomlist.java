bcfde4834ee3b2f7c63e24e85cea579a6304d842
lennox1 <lennox1@gmail.com>
2026-08-18 13:35:52 -0600

    Se agregaron los override en waitingroom

--- src/WaitingRooms/waitingroomlist.java
+++ src/WaitingRooms/waitingroomlist.java
@@ -4,11 +4,11 @@
  */
 package WaitingRooms;
 
+import java.util.Iterator;
 import java.util.Queue;
 import patients.Patient;
 
 /**
- *
  * @author Student
  */
 public class waitingroomlist implements SequentialDynamicsList<Patient> {
@@ -16,6 +16,36 @@ public class waitingroomlist implements SequentialDynamicsList<Patient> {
 
     public waitingroomlist(Queue<Patient> waitinList) {
         this.waitinList = waitinList;
+    }
+
+    @Override
+    public Patient get() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean remove() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean add(Patient item) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Iterator getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
 }
