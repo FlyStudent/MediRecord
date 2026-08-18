439f1999ce89279ab0c633975e077a35a14983f5
Bryan <bryanlaraarguello@gmail.com>
2026-08-18 12:14:32 -0600

    Pacienteslist listo

--- src/patients/PacientsList.java
+++ src/patients/PacientsList.java
@@ -1,54 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package patients;
 
 import appointments.KeyDynamicLists;
 import java.util.HashMap;
 import java.util.Iterator;
+
 /**
  *
  * @author bryan
  */
-public class PacientsList implements KeyDynamicLists<Patient,String> {
-    
-    private HashMap<String,Patient>patients;
+public class PacientsList implements KeyDynamicLists<Patient, String> {
 
-    public PacientsList(HashMap patients) {
-        this.patients = new HashMap();
+    private HashMap<String, Patient> patients;
+
+    public PacientsList() {
+        this.patients = new HashMap<>();
     }
 
     @Override
     public Patient get(String id) {
-      
+        try {
+            return patients.get(id);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override
     public boolean remove(String id) {
-        
+        return this.patients.remove(id) != null;
     }
 
     @Override
     public boolean add(Patient item) {
-        
+        if (patients.containsKey(item.getId())) return false;
+        return patients.put(item.getId(), item) == null;
     }
 
     @Override
-    public Iterator getAll() {
-       
+    public Iterator<Patient> getAll() {
+        return this.patients.values().iterator();
     }
 
     @Override
     public int size() {
-        
+        return this.patients.size();
     }
 
     @Override
     public boolean isEmpty() {
-       
+        return this.patients.isEmpty();
     }
-
-    
-    
 }
